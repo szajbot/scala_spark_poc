df427322f86cc640eacdb1219bb15ce662a6dac0
Tobiasz <Tobiasz@TOBIASZ-LAPTOP>
2025-02-04 13:14:39 +0100

    Added some temp data to csv files

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,10 +4,35 @@ import org.apache.spark.sql.SparkSession
 
 object Main {
   def main(args: Array[String]): Unit = {
+    val resourcesPath = "src/main/resources/"
+    val employeesFileName = "employees.csv"
+    val departmentsFileName = "departments.csv"
+    val transactionFileName = "transaction.csv"
+
     val spark = SparkSession
       .builder()
       .appName("Spark SQL basic example")
-      .config("spark.some.config.option", "some-value")
+      .master("local[*]")
       .getOrCreate()
+
+    val df_departments = spark.read
+      .option("delimiter", ";")
+      .option("header", value = true)
+      .csv(resourcesPath + departmentsFileName)
+
+    val df_employees = spark.read
+      .option("delimiter", ";")
+      .option("header", value = true)
+      .csv(resourcesPath + employeesFileName)
+
+    val df_transaction = spark.read
+      .option("delimiter", ";")
+      .option("header", value = true)
+      .csv(resourcesPath + transactionFileName)
+
+    df_departments.show()
+    df_employees.show()
+    df_transaction.show()
+
   }
 }
