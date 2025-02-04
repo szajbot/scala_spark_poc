cb29b9c047c02c91db91216b21da64131c776b0b
Tobiasz <Tobiasz@TOBIASZ-LAPTOP>
2025-02-04 17:09:53 +0100

    Refactored and changed logic for query

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -20,7 +20,6 @@ object Main {
       .option("delimiter", ";")
       .option("header", value = true)
       .csv(resources_path + employees_file_name)
-      .select(col("id"), col("department"))
       .filter(not(col("position").isin("junior")))
 
     val df_departments = spark.read
@@ -28,46 +27,45 @@ object Main {
       .option("header", value = true)
       .csv(resources_path + departments_file_name)
 
-    // Departments join employee to get employee count
-    val df_departments_prepared = df_departments.as("dd")
-      .join(df_employees.as("de"), df_departments("id") === df_employees("department"), "outer")
-      .groupBy(col("dd.id"), col("dd.name"))
-      .agg(countDistinct("de.id").as("employee_count"))
-
     val df_transaction = spark.read
       .option("delimiter", ";")
       .option("header", value = true)
       .csv(resources_path + transaction_file_name)
       .filter(not(col("type").isin("intermediary sale")))
 
-    val transaction_employees_join = df_transaction(
-      "employee"
-    ) === df_employees("id")
-
-    val df_joined = df_transaction.as("dt")
-      .join(df_employees.as("de"), transaction_employees_join, "inner")
-      .select(col("value"), col("type"), col("employee"), col("dt.id").as("transaction"), col("department"))
-
-    val employees_department_join = df_joined(
-      "department"
-    ) === df_departments_prepared("id")
-
-    val df_with_employee_count = df_joined.join(df_departments_prepared, employees_department_join, "outer")
-      .select(col("value").cast("int"), col("type"), col("employee"), col("name"), col("department"), col("employee_count"))
-
-    val df_with_normalized_value = df_with_employee_count.withColumn(
-      "normalized_value",
-      when(col("type") === "contract", col("value") * 3).otherwise(col("value"))
-    )
-
-    val df_for_report = df_with_normalized_value
-      .groupBy(col("department"), col("name"), col("employee_count"))
-      .sum("normalized_value")
+    val df_1 = df_transaction.as("dt")
+      .join(df_employees.as("de"), df_employees("id") === df_transaction("employee"), "inner")
+      .join(df_departments.as("dd"), df_departments("id") === df_employees("department"), "outer")
       .select(
-        col("name").as("Department name"),
-        when(col("sum(normalized_value)").isNotNull, col("sum(normalized_value)")).otherwise(0).as("average revenue per employee")
-      ).sort(col("average revenue per employee").desc)
+        col("dt.id").as("transaction"),
+        col("value").cast("int"),
+        col("type"),
+        col("employee"),
+        col("position"),
+        col("department"),
+        col("dd.name")
+      )
+//    Wyfiltorwani zostali pracownicy bez transakcji ale został department bez pracownika
 
-    df_for_report.show()
+    val df_with_employee_count = df_1.groupBy(col("department")).agg(countDistinct(col("employee")))
+
+    val df_with_summed_values = df_1
+      .select(
+        col("name"),
+        when(col("type") === "contract", col("value") * 6).otherwise(col("value")).as("normalized value"),
+        col("department")
+      ).groupBy(col("name"), col("department")).sum("normalized value")
+
+    val df_sums_and_employees = df_with_summed_values
+      .join(df_with_employee_count, df_with_summed_values.col("department") === df_with_employee_count.col("department"), "left_outer")
+
+    val df_report = df_sums_and_employees.select(
+      col("name"),
+      when(col("sum(normalized value)").isNull.or(col("count(DISTINCT employee)") === 0), 0).otherwise(
+        col("sum(normalized value)") / col("count(DISTINCT employee)")
+      ).as("average revenue per employee")
+    ).sort(col("average revenue per employee").desc)
+
+    df_report.show()
   }
 }
